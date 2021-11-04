366b2e9a65dd0ab4b6c5e78a611da4656a61cb16
Muhammad Mubbashir Hassan <mm=hassan>
2021-11-04 11:01:37 +0500

    edit

---
+++ server.mjs
@@ -0,0 +1,17 @@
+import express from 'express';
+const app =express();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("I am a hello world server");
+})
+app.get("/profile", (req, res) => {
+    res.send("here is your profile");
+})
+app.get("/about", (req, res) => {
+    res.send("some information about me");
+})
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+});
